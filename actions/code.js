80d6b3b1d709456e32271af55079b317f5623d9d
zubairov <renat.zubairov@elastic.io>
2015-10-02 15:06:38 +0200

    First ES6 tests are working

--- actions/code.js
+++ actions/code.js
@@ -1,25 +1,31 @@
-/*global console */
+"use strict";
 var vm = require('vm');
 var Q = require('q');
 var elasticio = require('elasticio-node');
-var messages = elasticio.messages;
+var debug = require('debug')('code');
 
 exports.process = function (msg, conf) {
-
-    var context = {
-        msg:msg,
-        require: require,
-        console: console,
-        messages : messages,
-        elasticio: elasticio,
-        Q : Q,
-        self : this
-    };
-
-    try {
-        vm.runInNewContext(conf.code, context);
-    } catch (error) {
-        this.emit('error', error);
+    var ctx = vm.createContext({
+        console : console,
+        process : process,
+        require : require,
+        setTimeout : setTimeout,
+        clearTimeout: clearTimeout,
+        setInterval : setInterval,
+        clearInterval: clearInterval,
+        exports : {},
+        messages : elasticio.messages
+    });
+    debug('Running the code %s', conf.code);
+    vm.runInContext(conf.code, ctx, {
+        displayErrors : true
+    });
+    debug("No result, let's check the run object if it was created?");
+    if (ctx.run && typeof ctx.run.apply == 'function') {
+        debug("Run variable is a function, calling it");
+        var result = ctx.run.apply(this, [msg]);
+    } else {
+        debug("Run function was not found, it's over now");
+        this.emit('end');
     }
-    this.emit('end');
 };
---
+++ spec/code.spec.js
@@ -0,0 +1,74 @@
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
+function TaskExec() {
+    EventEmitter.call(this);
+    this.errorCount = 0;
+}
+util.inherits(TaskExec, EventEmitter);
+var fn = require('../actions/code');
+function testFn(code, cb) {
+    var executor = new TaskExec();
+    executor.data = [];
+    executor.on('end', function () {
+        "use strict";
+        cb(executor);
+    });
+    executor.on('data', function (value) {
+        "use strict";
+        this.data.push(value);
+    });
+    fn.process.apply(executor, [{}, {
+        code: code
+    }]);
+}
+
+describe('ES5 code block', function() {
+    "use strict";
+
+
+    it('Processes hello code normally', function (done) {
+        testFn("console.log('Hello code!');", function () {
+            "use strict";
+            done();
+        });
+    });
+
+    it('Processes hello code with run function', function (done) {
+        testFn("function run(msg) { console.log('Hello code!'); this.emit('end');}", function () {
+            "use strict";
+            done();
+        });
+    });
+
+
+    it('Processes hello code emitting', function (done) {
+        testFn("function run(message) {" +
+            "this.emit('data', messages.newMessageWithBody({message: 'hello world'}));" +
+            "this.emit('end'); " +
+            "}",
+            function (executor) {
+                "use strict";
+                expect(executor.data.length).toEqual(1);
+                done();
+            });
+    });
+
+});
+
+xdescribe('ES6 Code Block', function () {
+
+    it('Promise that resolves', function (done) {
+        "use strict";
+        testFn("function run(message) {" +
+            "return new Promise(function(resolve, reject) {" +
+            "resolve(true);" +
+            "})" +
+            "};", function (executor) {
+            expect(executor.data.length).toEqual(1);
+            done();
+        });
+    });
+
+
+});
