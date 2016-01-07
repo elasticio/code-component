da0234541c1f4007c28ad739c1d1a678687ddec1
Igor Drobiazko <igor@elastic.io>
2016-01-07 15:57:59 +0100

    Removed docker cp

--- .circleci/build_slug.sh
+++ .circleci/build_slug.sh
@@ -2,7 +2,6 @@
 echo "Building slug"
 id=$(git archive $CIRCLE_BRANCH | docker run -e "NPM_CONFIG_PRODUCTION=false" -i -a stdin elasticio/appbuilder)
 docker attach $id
-docker cp $id:/tmp/slug.tgz $CIRCLE_ARTIFACTS
 RC=$?
 if [ $RC -eq 0 ];then
   echo "Build ok."
