11e84844febf42064a735a59f5c9e206c5f271f4
Olha Virolainen <olga.virolainen@elastic.io>
2019-08-27 14:43:48 +0300

    support async function (#25)
    * support async function

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+## 1.0.1 (August 28, 2019)
+
+Fixed bug:
+* Nodejs code component processes all at once messages in it's input queue in case of asynchronous code body.
+
+## 1.0.0 (August 2, 2019)
+
+* Initial release which includes a bunch of previous unversioned releases
+
--- actions/code.js
+++ actions/code.js
@@ -1,67 +1,69 @@
-'use strict';
+// eslint-disable-next-line import/no-extraneous-dependencies
+const _ = require('lodash');
 const vm = require('vm');
-const elasticio = require('elasticio-node');
+const { messages } = require('elasticio-node');
 const co = require('co');
 const request = require('co-request');
-const _ = require('lodash');
 
 function wait(timeout) {
-    return new Promise(ok => {
-        setTimeout(() => {
-            console.log('Done wait');
-            ok();
-        }, timeout);
-        this.logger.info('Start wait sec=%s', timeout);
-    });
+  return new Promise((ok) => {
+    setTimeout(() => {
+      this.logger.info('Done wait');
+      ok();
+    }, timeout);
+    this.logger.info('Start wait sec=%s', timeout);
+  });
 }
 
+// eslint-disable-next-line consistent-return,func-names
 exports.process = async function (msg, conf) {
-    const ctx = vm.createContext({
-        _: _,
-        console: console,
-        process: process,
-        require: require,
-        setTimeout: setTimeout,
-        clearTimeout: clearTimeout,
-        setInterval: setInterval,
-        clearInterval: clearInterval,
-        msg: msg,
-        exports: {},
-        messages: elasticio.messages,
-        request: request,
-        wait: wait.bind(this),
-        emitter: this
-    });
-    this.logger.info('Running the code %s', conf.code);
-    vm.runInContext(conf.code, ctx, {
-        displayErrors: true
-    });
-    this.logger.info("No result, let's check the run object if it was created?");
-    if (ctx.run && typeof ctx.run.apply == 'function') {
-        let result;
-        if (ctx.run.constructor.name === 'GeneratorFunction') {
-            this.logger.info('Run variable is a generator');
-            result = co(ctx.run);
-        } else {
-            this.logger.info("Run variable is a function, calling it");
-            result = ctx.run.apply(this, [msg]);
-        }
-        if (typeof result === 'object' && typeof result.then === 'function') {
-            this.logger.info('Returned value is a promise, will evaluate it');
-            result.then(resolved => {
-                this.logger.info('Promise resolved');
-                if (resolved) {
-                    this.emit('data', elasticio.messages.newMessageWithBody(resolved));
-                }
-                return this.emit('end');
-            }).catch(async err => {
-                this.logger.info('Promise failed', err);
-                await this.emit('error', err);
-                this.emit('end');
-            });
-        }
+  const ctx = vm.createContext({
+    _,
+    console,
+    process,
+    require,
+    setTimeout,
+    clearTimeout,
+    setInterval,
+    clearInterval,
+    msg,
+    exports: {},
+    messages,
+    request,
+    wait: wait.bind(this),
+    emitter: this,
+  });
+  this.logger.info('Running the code %s', conf.code);
+  vm.runInContext(conf.code, ctx, {
+    displayErrors: true,
+  });
+  this.logger.info("No result, let's check the run object if it was created?");
+  if (ctx.run && typeof ctx.run.apply === 'function') {
+    let result;
+    if (ctx.run.constructor.name === 'GeneratorFunction') {
+      this.logger.info('Run variable is a generator');
+      result = co(ctx.run);
     } else {
-        this.logger.info("Run function was not found, it's over now");
-        this.emit('end');
+      this.logger.info('Run variable is a function, calling it');
+      result = ctx.run.apply(this, [msg]);
     }
+    if (typeof result === 'object' && typeof result.then === 'function') {
+      this.logger.info('Returned value is a promise, will evaluate it');
+      let returnResult;
+      try {
+        returnResult = await result;
+        this.logger.info('Promise resolved');
+        if (returnResult) {
+          return messages.newMessageWithBody(returnResult);
+        }
+        this.emit('end');
+      } catch (e) {
+        this.logger.error('Promise failed', e);
+        throw e;
+      }
+    }
+  } else {
+    this.logger.info("Run function was not found, it's over now");
+    this.emit('end');
+  }
 };
